3f0f00426ae9933e4a948e700ed05b81d36c5f7f
hugostenarson <hugo.stenarson@elev.ga.ntig.se>
2024-10-10 14:57:41 +0200

    Klar

---
+++ Arsenal/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+
+string contents = File.ReadAllText("weapon.json");
+
+//var weapon = JsonSerializer.Deserialize<Weapon>(contents)
+if (JsonSerializer.Deserialize<Weapon>(contents) is not Weapon w) {
+    Console.WriteLine("Error!");
+    return;
+}
+//Weapon w = JsonSerializer.Deserialize<Weapon>(contents);
+
+Console.WriteLine("Hur många attacker?");
+
+int numAttacks = 0;
+string numAttacksStr = "";
+
+while (!int.TryParse(numAttacksStr, out numAttacks))
+{
+    if (Console.ReadLine() is string inputString) {
+        numAttacksStr = inputString;
+    }
+}
+
+int damageSum = 0;
+
+Console.ForegroundColor = ConsoleColor.Red;
+
+for (int i = 0; i < numAttacks; i++)
+{
+    int damage = w.Attack();
+    Console.WriteLine($"+{damage}");
+    damageSum += damage;
+}
+
+Console.ResetColor();
+
+Console.WriteLine($"Du gjorde totalt {damageSum} skada!");
+
+
+Console.ReadLine();
---
+++ Arsenal/Weapon.cs
@@ -0,0 +1,16 @@
+using System.Text.Json.Serialization;
+
+public class Weapon {
+    [JsonInclude]
+    public string Name = "";
+
+    [JsonInclude]
+    public int DamageMin;
+
+    [JsonInclude]
+    public int DamageMax;
+
+    public int Attack() {
+        return Random.Shared.Next(DamageMin, DamageMax);
+    }
+}
